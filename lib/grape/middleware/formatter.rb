fa41f5d7a5c49fea78e764856d06a66e98856ae8
dblock <dblock@dblock.org>
2013-01-11 00:00:28 -0500

    Fallback to api.format.

--- lib/grape/middleware/formatter.rb
+++ lib/grape/middleware/formatter.rb
@@ -35,10 +35,10 @@ module Grape
 
         def read_body_input
           request_method = request.request_method.to_s.upcase
-          if ENV['CONTENT_TYPE'] && [ 'POST', 'PUT' ].include?(request_method) && (! request.form_data?) && (request.content_length.to_i > 0)
+          if [ 'POST', 'PUT' ].include?(request_method) && (! request.form_data?) && (request.content_length.to_i > 0)
             if env['rack.input'] && (body = env['rack.input'].read).strip.length > 0
               begin
-                fmt = mime_types[format_from_content_type]
+                fmt = mime_types[format_from_content_type || env['api.format']]
                 if content_type_for(fmt)
                   parser = Grape::Parser::Base.parser_for fmt, options
                   unless parser.nil?
